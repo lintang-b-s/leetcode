2bae420d0aba1c2abbc329203004172717864443
Lintang B S <109817421+lintang-b-s@users.noreply.github.com>
2023-09-03 09:59:46 +0000

    Sync LeetCode submission - Is Subsequence (java)

---
+++ my-folder/problems/is_subsequence/solution.java
@@ -0,0 +1,25 @@
+
+/*
+
+abc  ahbgdc
+i    j
+abc  ahbgdc
+ i      j
+abc  ahbgdc
+  i       j
+  return i == n;
+*/
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i= 0, j = 0;
+        while (i < s.length() && j < t.length()){
+            if (s.charAt(i)== t.charAt(j)){
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    }
+}
