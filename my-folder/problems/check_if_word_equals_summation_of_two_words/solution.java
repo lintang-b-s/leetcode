e3d64ecc0a6f84b777c80fed56d17c5006efe67a
Lintang B S <109817421+lintang-b-s@users.noreply.github.com>
2023-09-06 02:18:55 +0000

    Sync LeetCode submission - Check if Word Equals Summation of Two Words (java)

---
+++ my-folder/problems/check_if_word_equals_summation_of_two_words/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+        return getIntValue(firstWord) + getIntValue(secondWord) == getIntValue(targetWord);
+    }
+    private int getIntValue(String s) {
+        int result = 0;
+        for(int i=0;i<s.length();i++)
+            result = result * 10 + (s.charAt(i) - 'a');
+        return result;
+    }
+}
