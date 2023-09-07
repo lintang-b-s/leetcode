00772ea9282adf7b888b94376e2996b3b4e26e06
Lintang B S <109817421+lintang-b-s@users.noreply.github.com>
2023-09-07 05:08:14 +0000

    Sync LeetCode submission - Game Play Analysis IV (mysql)

---
+++ my-folder/problems/game_play_analysis_iv/solution.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT ROUND(
+    COUNT(A1.player_id)
+    / (SELECT COUNT(DISTINCT A3.player_Id) FROM Activity A3),2) as fraction
+FROM Activity A1
+WHERE
+  (A1.player_id, DATE_SUB(A1.event_date, INTERVAL 1 DAY)) IN (
+    SELECT
+      A2.player_id,
+      MIN(A2.event_date)
+    FROM
+      Activity A2
+    GROUP BY
+      A2.player_id
+  );
