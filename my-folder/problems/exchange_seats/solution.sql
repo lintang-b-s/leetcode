403df93534443cfbef4ba1a76218352dd93d1eb6
Lintang B S <109817421+lintang-b-s@users.noreply.github.com>
2023-09-08 09:17:17 +0000

    Sync LeetCode submission - Exchange Seats (mysql)

---
+++ my-folder/problems/exchange_seats/solution.sql
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+SELECT (
+    CASE
+        WHEN MOD(id, 2) != 0 AND counts != id THEN id + 1
+        WHEN MOD(id, 2) != 0 AND counts = id THEN id
+        ELSE id-1
+    END )AS id,
+    student
+FROM 
+    seat,
+    (SELECT COUNT(*)  as counts
+    FROM seat ) as seat_counts 
+ORDER BY id;
+
+
+
+
+
+
+
+
